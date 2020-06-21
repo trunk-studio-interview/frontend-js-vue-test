c0ec4deee45fc74d1ddf93aad5b081bb27b4cb49
smlsunxie <smlsun@gmail.com>
2020-06-22 06:38:28 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    auth: {},
   },
   mutations: {
+    USER_LOGIN(state, auth) {
+      console.log('auth', auth);
+      state.auth = auth;
+    },
   },
   actions: {
+    login({ commit }) {
+      axios
+        .post('/api/auth/login', { email: 'admin@example.com', password: 'admin' })
+        .then((result) => {
+          commit('USER_LOGIN', result.data);
+        })
+        .catch((error) => {
+          throw new Error(`API ${error}`);
+        });
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,43 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <table>
+      <tr>
+        <th>access_token</th>
+        <th>token_type</th>
+        <th>expires_in</th>
+      </tr>
+      <tr>
+        <td>{{auth.access_token}}</td>
+        <td>{{auth.token_type}}</td>
+        <td>{{auth.expires_in}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import { mapState } from 'vuex';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
   },
+
+  created() {
+    this.$store.dispatch('login');
+  },
+  computed: mapState(['auth']),
 };
 </script>
+<style scoped lang="scss">
+table {
+  width:100%;
+  table-layout: fixed;
+  overflow-wrap: break-word;
+}
+</style>
